339564a6aeda0610b748e4060220800bb2221c47
chenchen34 <2269412240@qq.com>
2019-11-02 17:49:31 +0800

    开发项目管理-详情-附件列表

--- src/app/business-modules/xmgl/kfxmgl/detail/kfxmgl-detail.component.ts
+++ src/app/business-modules/xmgl/kfxmgl/detail/kfxmgl-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewChildren, QueryList } from '@angular/core';
 import { NzMessageService } from 'ng-zorro-antd';
+import { Router , ActivatedRoute} from '@angular/router';
 import { ValidationDirective } from 'src/app/layout/_directives/validation.directive';
 import * as Moment from 'moment';
 import * as $ from 'jquery';
@@ -16,22 +17,133 @@ export class KfxmglDetailComponent implements OnInit {
     {name:'项目信息',index:0},
     {name:'附件',index:1},
   ]
-  tabsetIndex = 0;
+  tabsetIndex = 1;
+  isDisable = false;
+  detailId = "";
   detailObj:any = {};
+  selectId = -1;
+  fjList = [
+    {name:"f",scrq:'4545',id:1},
+    {name:"fsf",scrq:'454',id:2},
+    {name:"gh",scrq:'554',id:3},
+    {name:"hk",scrq:'453',id:4},
+    {name:";;",scrq:'3234',id:5},
+    {name:"er",scrq:'24',id:6},
+    {name:"f",scrq:'4545',id:1},
+    {name:"fsf",scrq:'454',id:2},
+    {name:"gh",scrq:'554',id:3},
+    {name:"hk",scrq:'453',id:4},
+    {name:";;",scrq:'3234',id:5},
+    {name:"er",scrq:'24',id:6},
+    {name:"f",scrq:'4545',id:1},
+    {name:"fsf",scrq:'454',id:2},
+    {name:"gh",scrq:'554',id:3},
+    {name:"hk",scrq:'453',id:4},
+    {name:";;",scrq:'3234',id:5},
+    {name:"er",scrq:'24',id:6}
+  ];
+  pageIndex: any = 1;
+  totalCount: any;
+  pageSize: any = 10;
+  Loading = false;
+  tableIsScroll = null;
+  isAllDisplayDataChecked = false;
+  isIndeterminate = false;
+  listOfDisplayData = [];
+  listOfAllData = [];
+  mapOfCheckedId: { [key: string]: boolean } = {};
+  numberOfChecked = 0;
 
   constructor(
-    private msg: NzMessageService
-  ) { }
+    private msg: NzMessageService,
+    private router:Router,
+    private activatedRoute:ActivatedRoute
+  ) {
+    var type = this.activatedRoute.snapshot.queryParams.type;
+    var id = this.activatedRoute.snapshot.queryParams.id;
+
+    switch (type) {
+      case '1'://添加
+        this.isDisable = false;
+        break;
+      case '2'://查看
+        this.isDisable = true;
+        break;
+      case '3'://编辑
+        this.isDisable = false;
+        break;
+      default:
+        break;
+    }
+    
+   }
 
   ngOnInit() {
+    this.search();
+  }
 
+  search(){
+    this.calculationHeight();
+    this.operateData();
   }
 
   tabsetChange(m){
     this.tabsetIndex = m;
   }
+  cancel(){
+    this.router.navigate(['/xmgl/kfxmgl']);
+  }
+
+  pageIndexChange(num) {
+    this.pageIndex = num;
+    this.search();
+  }
+
+  pageSizeChange(num) {
+    this.pageSize = num;
+    this.pageIndex = 1;
+    this.search();
+  }
+
+  currentPageDataChange($event): void {
+    this.listOfDisplayData = $event;
+    this.refreshStatus();
+  }
+
+  refreshStatus(): void {
+    this.isAllDisplayDataChecked = this.listOfDisplayData
+      .filter(item => !item.disabled)
+      .every(item => this.mapOfCheckedId[item.id]);
+    this.isIndeterminate =
+      this.listOfDisplayData.filter(item => !item.disabled).some(item => this.mapOfCheckedId[item.id]) &&
+      !this.isAllDisplayDataChecked;
+    this.numberOfChecked = this.listOfAllData.filter(item => this.mapOfCheckedId[item.id]).length;
+
+    for(var id in this.mapOfCheckedId){
+        console.log(id)
+    }
+  }
+
+  checkAll(value: boolean): void {
+    this.listOfDisplayData.filter(item => !item.disabled).forEach(item => (this.mapOfCheckedId[item.id] = value));
+    this.refreshStatus();
+  }
+
+  operateData(): void {
+    setTimeout(() => {
+      this.listOfAllData.forEach(item => (this.mapOfCheckedId[item.id] = false));
+      this.refreshStatus();
+    }, 1000);
+  }
 
 
+  onChange(m,date){
+    if(m == 1){
+      this.detailObj.kgrq = Moment(date).format('YYYY-MM-DD')
+    }else if(m == 2){
+      this.detailObj.jgrq = Moment(date).format('YYYY-MM-DD')
+    }
+  }
 
   FormValidation() {
     let isValid = true;
@@ -43,14 +155,30 @@ export class KfxmglDetailComponent implements OnInit {
     return isValid;
   }
 
+  selectItem(data) {
+    this.selectId = data.id;
+  }
 
 
   save(){
     this.FormValidation();
   }
 
+  calculationHeight(){
+    const bodyHeight = $('body').height()
+    const height = this.fjList.length * 40;
+    if(height > bodyHeight - 400){
+        this.tableIsScroll = {y: bodyHeight - 400 + 'px'}
+    }else{
+      this.tableIsScroll = null
+    }
+  }
+
   ngAfterViewInit() {
-    
+    var that = this;
+    $(window).resize(function () {
+      that.calculationHeight()
+    })
   }
 
 }
