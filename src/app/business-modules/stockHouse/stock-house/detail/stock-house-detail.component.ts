33eaaa0bd47457d7c1c3ef71757780dc0de63966
jianl123 <514295560@qq.com>
2019-11-03 23:09:30 +0800

    修改存量房附件

--- src/app/business-modules/stockHouse/stock-house/detail/stock-house-detail.component.ts
+++ src/app/business-modules/stockHouse/stock-house/detail/stock-house-detail.component.ts
@@ -6,6 +6,7 @@ import * as Moment from 'moment';
 import * as $ from 'jquery';
 import { StockHouseService } from "../../../service/stockHouse/stock-house.service";
 import { Localstorage } from '../../../service/localstorage';
+import { FileService } from '../../../service/file/file.service';
 
 @Component({
   selector: 'app-stock-house-detail',
@@ -16,7 +17,7 @@ export class StockHouseDetailComponent implements OnInit {
 
   @ViewChildren(ValidationDirective) directives: QueryList<ValidationDirective>;
   @ViewChild('uploadComponent', { static: false }) uploadComponent;
-
+  downLoadurl = AppConfig.Configuration.baseUrl + "/FileInfo/download";
   tabs = [
     { name: '项目信息', index: 0 },
     { name: '附件', index: 1 },
@@ -24,30 +25,11 @@ export class StockHouseDetailComponent implements OnInit {
   tabsetIndex = 0;
   isDisable = false;
   detailId = "";
-  detailObj: any={
-    relationShips:[]
+  detailObj: any = {
+    relationShips: []
   };
   selectId = -1;
-  fjList = [
-    { name: "f", scrq: '4545', id: 1 },
-    { name: "fsf", scrq: '454', id: 2 },
-    { name: "gh", scrq: '554', id: 3 },
-    { name: "hk", scrq: '453', id: 4 },
-    { name: ";;", scrq: '3234', id: 5 },
-    { name: "er", scrq: '24', id: 6 },
-    { name: "f", scrq: '4545', id: 1 },
-    { name: "fsf", scrq: '454', id: 2 },
-    { name: "gh", scrq: '554', id: 3 },
-    { name: "hk", scrq: '453', id: 4 },
-    { name: ";;", scrq: '3234', id: 5 },
-    { name: "er", scrq: '24', id: 6 },
-    { name: "f", scrq: '4545', id: 1 },
-    { name: "fsf", scrq: '454', id: 2 },
-    { name: "gh", scrq: '554', id: 3 },
-    { name: "hk", scrq: '453', id: 4 },
-    { name: ";;", scrq: '3234', id: 5 },
-    { name: "er", scrq: '24', id: 6 }
-  ];
+  fjList = [];
   pageIndex: any = 1;
   totalCount: any;
   pageSize: any = 10;
@@ -60,7 +42,10 @@ export class StockHouseDetailComponent implements OnInit {
   mapOfCheckedId: { [key: string]: boolean } = {};
   numberOfChecked = 0;
   isVisible = false;
-  genderList=[];
+  genderList = [];
+
+  isImgVisible = false;
+  currentImg = "";
 
   constructor(
     private msg: NzMessageService,
@@ -68,13 +53,10 @@ export class StockHouseDetailComponent implements OnInit {
     private activatedRoute: ActivatedRoute,
     private stockHouseService: StockHouseService,
     private localstorage: Localstorage,
+    private fileService: FileService
   ) {
-
-  }
-
-  ngOnInit() {
     var type = this.activatedRoute.snapshot.queryParams.type;
-    var id = this.activatedRoute.snapshot.queryParams.id;
+    this.detailObj.id = this.activatedRoute.snapshot.queryParams.id;
 
     switch (type) {
       case '1'://添加
@@ -89,9 +71,14 @@ export class StockHouseDetailComponent implements OnInit {
       default:
         break;
     }
+  }
+
+  ngOnInit() {
+
     this.getDictory();
-    if (id) {
-     this.getStockHouseById(id);
+    if (this.detailObj.id) {
+      this.getStockHouseById();
+      this.search();
     }
   }
 
@@ -101,15 +88,27 @@ export class StockHouseDetailComponent implements OnInit {
     this.genderList = dic.gender;
   }
 
-  async getStockHouseById(id) {
+  async getStockHouseById() {
 
-    let data = await this.stockHouseService.getStockHouseById(id);
-    if (data) {
+    let data = await this.stockHouseService.getStockHouseById(this.detailObj.id);
+    if (data&&data.code==200) {
       this.detailObj = data.msg;
-      if(!this.detailObj.relationShips){
-        this.detailObj.relationShips=[];
+      if (!this.detailObj.relationShips) {
+        this.detailObj.relationShips = [];
       }
+    } else {
+      this.msg.create('error', '内部服务出错');
     }
+  }
+
+  async search() {
+    var res = await this.fileService.getFileListById(this.detailObj.id);
+    if (res && res.code == 200) {
+      this.fjList = res.msg;
+    }
+
+    this.calculationHeight();
+    this.operateData();
   }
 
 
@@ -118,6 +117,57 @@ export class StockHouseDetailComponent implements OnInit {
   }
   cancel() {
     this.router.navigate(['/stockHouse']);
+  }
+
+  pageIndexChange(num) {
+    this.pageIndex = num;
+    this.search();
+  }
+
+  pageSizeChange(num) {
+    this.pageSize = num;
+    this.pageIndex = 1;
+    this.search();
+  }
+
+  currentPageDataChange($event): void {
+    this.listOfDisplayData = $event;
+    this.refreshStatus();
+  }
+
+  refreshStatus(): void {
+    this.isAllDisplayDataChecked = this.listOfDisplayData
+      .filter(item => !item.disabled)
+      .every(item => this.mapOfCheckedId[item.id]);
+    this.isIndeterminate =
+      this.listOfDisplayData.filter(item => !item.disabled).some(item => this.mapOfCheckedId[item.id]) &&
+      !this.isAllDisplayDataChecked;
+    this.numberOfChecked = this.listOfAllData.filter(item => this.mapOfCheckedId[item.id]).length;
+
+    for (var id in this.mapOfCheckedId) {
+      console.log(id)
+    }
+  }
+
+  checkAll(value: boolean): void {
+    this.listOfDisplayData.filter(item => !item.disabled).forEach(item => (this.mapOfCheckedId[item.id] = value));
+    this.refreshStatus();
+  }
+
+  operateData(): void {
+    setTimeout(() => {
+      this.listOfAllData.forEach(item => (this.mapOfCheckedId[item.id] = false));
+      this.refreshStatus();
+    }, 1000);
+  }
+
+
+  onChange(m, date) {
+    // if(m == 1){
+    //   this.detailObj.kgrq = Moment(date).format('YYYY-MM-DD')
+    // }else if(m == 2){
+    //   this.detailObj.jgrq = Moment(date).format('YYYY-MM-DD')
+    // }
   }
 
   FormValidation() {
@@ -150,16 +200,16 @@ export class StockHouseDetailComponent implements OnInit {
     }
   }
 
-  addpeople(){
-    if(!this.detailObj.relationShips){
-      this.detailObj.relationShips=[];
+  addpeople() {
+    if (!this.detailObj.relationShips) {
+      this.detailObj.relationShips = [];
     }
-    var newpeople={};
+    var newpeople = {};
     this.detailObj.relationShips.push(newpeople);
   }
 
-  deletepeople(obj,i){
-    this.detailObj.relationShips.splice(i,1);
+  deletepeople(obj, i) {
+    this.detailObj.relationShips.splice(i, 1);
   }
 
   calculationHeight() {
@@ -186,6 +236,76 @@ export class StockHouseDetailComponent implements OnInit {
   handleOk() {
     this.uploadComponent.import();
   }
+
+  outer(event) {
+    if (event) {
+      this.handleCancel();
+      this.search();
+    }
+  }
+
+  previewImg(item) {
+    if (item.fileSuffix != 'pdf') {
+      this.currentImg = this.downLoadurl + "?id=" + item.id + "&type=0";
+      this.isImgVisible = true;
+    } else {
+      window.open(this.downLoadurl + "?id=" + item.id + "&type=0");
+    }
+
+  }
+
+  //删除
+  async btachDelete(item) {
+    var ids = [];
+    if (item) {//单个删除
+      ids.push(item.id);
+    } else {//批量删除
+      if (this.listOfDisplayData.length > 0) {
+        this.listOfDisplayData.forEach(element => {
+          if (this.mapOfCheckedId[element.id]) {
+            ids.push(element.id);
+          }
+        });
+      }
+    }
+
+    if (ids.length == 0) {
+      this.msg.warning('请选择需要删除的项目');
+      return;
+    }
+
+    var res = await this.fileService.delete(item.id);
+      if (res && res.code == 200) {
+        this.msg.create('success', '删除成功');
+        this.search();
+      } else {
+        this.msg.create('error', '删除失败');
+      }
+  }
+
+  //下载
+  btachDown(item) {
+    var ids = [];
+    if (item) {//单个
+      ids.push(item.id);
+    } else {//批量
+      if (this.listOfDisplayData.length > 0) {
+        this.listOfDisplayData.forEach(element => {
+          if (this.mapOfCheckedId[element.id]) {
+            ids.push(element.id);
+          }
+        });
+      }
+    }
+
+    if (ids.length == 0) {
+      this.msg.warning('请选择需要下载的项目');
+      return;
+    }
+
+    window.location.href = this.downLoadurl + "?id=" + item.id + "&type=0";
+  }
+
 
   ngAfterViewInit() {
     var that = this;
